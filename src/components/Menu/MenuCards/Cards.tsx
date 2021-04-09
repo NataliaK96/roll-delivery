f7b7875eb41c8d6860a54accb1c4e288176f2f74
NataliaK96 <ng_komarova@mail.ru>
2021-04-09 21:19:28 +0400

    fix: rename card css

--- src/components/Menu/MenuCards/Cards.tsx
+++ src/components/Menu/MenuCards/Cards.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Card } from 'antd';
-import './cards.css';
+import './Cards.css';
 import { Count } from '../../Count/Count';
 import { observer } from 'mobx-react';
 import {store} from '../../../store'
